f77a389311b6f907e6ce5e06c9fb8853a55d4274
unknown <sergiocuadrado14@gmail.com>
2021-11-03 15:42:13 +0100

    Adding a mysql database online (clever-cloud)

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,23 @@
 // Init database
 module.exports = {
   database: {
+    host: 'bfak7zgxhlogu3jn5547-mysql.services.clever-cloud.com',
+    user: 'utgeh00li5f02wgh',
+    password: 'AgnelSm3hogQqGZCtmj8',
+    database: 'bfak7zgxhlogu3jn5547',
+  },
+};
+
+
+// Local database
+/* 
+module.exports = {
+  database: {
+    connectionLimit: 10,
     host: 'localhost',
     user: 'root',
     password: '1234',
     database: 'database_myanimemangalist',
   },
 };
+*/
